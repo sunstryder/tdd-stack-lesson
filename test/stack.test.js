8fd201c9ddf0268022dd90bc0e6184f9b7209103
Tony Sun <tsun@zendesk.com>
2021-12-20 16:39:30 +1100

    wip: add more features to stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,40 +1,74 @@
+// Create a stack data structure in JS without using arrays.
+// First in first out.
+// Needs the following methods:
+// push
+// pop
+// length
+// peek
+// clear
+
 class Stack {
   constructor() {
     this.top = -1;
     this.items = {};
-  }
+    // example items:
+    // const items = {
+    //   0: 'a',
+    //   1: 'b',
+    //   2: 'c'
+    // };
+}
 
-  get peek() {
-    return this.items[this.top];
-  }
-
-  push(value) {
-    this.top += 1;
+  push(value)
+  {
+    this.top++;
     this.items[this.top] = value;
+  } 
+
+  pop() { 
+    this.top--;
+    delete this.items[this.top]
   }
 }
 
-describe('My Stack', () => {
+describe('Stack', () => {
   let stack;
 
   beforeEach(() => {
     stack = new Stack();
   });
 
-  it('is created empty', () => {
-    expect(stack.top).toBe(-1);
+  // top
+  it('has a reference to top item', () => { 
+    expect(stack.top).toEqual(-1);
+  })
+
+  // items
+  it('holds a collection of all items', () => {
     expect(stack.items).toEqual({});
+  })
+
+  // push
+  it('can push to top of stack', () => {
+    stack.push('a');
+    expect(Object.keys(stack.items).length).toStrictEqual(1);
+    expect(stack.items[0]).toEqual("a");
   });
+  
+  // pop
+  it('can pop off top of stack', () => {
+    stack.push('a');
+    stack.pop();
+    expect(stack.top).toEqual(-1);
 
-  it('can push to the top', () => {
-    stack.push('🥑');
-    expect(stack.top).toBe(0);
-    expect(stack.peek).toBe('🥑');
-
-    stack.push('🌽');
-    expect(stack.top).toBe(1);
-    expect(stack.peek).toBe('🌽');
-  });
-
-  it.todo('can pop off');
+   });
+  
+// length
+  it.todo('can check length of stack')
+  
+// peek
+  it.todo('can get top of stack')
+  
+// clear
+  it.todo('can delete entire stack')
 });
